42bb495380a772f80d73d5dc9f146f0d0d000b83
Paruj Labpattanpaisan <paruj.lab@Parujs-MacBook-Pro.local>
2024-04-11 03:37:13 +0700

    feat:create getById getPagination api of account done

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	// _ "github.com/lib/pq" => have to use for connect db
+	_ "github.com/lib/pq"
+	"github.com/Kiminoto1412/simplebank/api"
+	db "github.com/Kiminoto1412/simplebank/db/sqlc"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connec the database")
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+
+	if err != nil{
+		log.Fatal("cannot start server:",err)
+	}
+}
